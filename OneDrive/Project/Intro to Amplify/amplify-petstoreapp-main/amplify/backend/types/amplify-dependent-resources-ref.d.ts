d97b9acc994823b41f8a4fcac409beb281768d38
natebelete1 <natebelete@gmail.com>
2023-09-13 11:07:52 -0700

    first commit

---
+++ OneDrive/Project/Intro to Amplify/amplify-petstoreapp-main/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,25 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifypetstore": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "amplifypetstore": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3amplifypetstoreappstorage4e8baa35": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
